7891b53d1dd3dfbd06fbebe179f5d748b3853b2a
Surendra-Reddy2216 <reddy110522@gmail.com>
2025-08-20 14:44:59 +0530

    my changes to project

--- Day-4/main.tf
+++ Day-4/main.tf
@@ -2,14 +2,13 @@ provider "aws" {
   region = "us-east-1"
 }
 
-resource "aws_instance" "abhishek" {
-  instance_type = "t2.micro"
-  ami = "ami-053b0d53c279acc90" # change this
-  subnet_id = "subnet-019ea91ed9b5252e7" # change this
+resource "aws_instance" "chaithu" {
+  instance_type = "t3.micro"
+  ami = "ami-00ca32bbc84273381" # change this
 }
 
 resource "aws_s3_bucket" "s3_bucket" {
-  bucket = "abhishek-s3-demo-xyz" # change this
+  bucket = "chaithu-s3-demo-xyz" # change this
 }
 
 resource "aws_dynamodb_table" "terraform_lock" {
